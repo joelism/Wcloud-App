fba42f21ec29fd4c0be6781eb45ee9fbf67dd67a
joelism <joel.weber1107@gmail.com>
2025-11-27 23:25:31 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="wcloud-cache-v1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json","./logo.png","./icons/icon-180x180.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
-self.addEventListener("fetch",e=>{ if(e.request.method!=="GET") return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
