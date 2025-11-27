e6ab2b888715cc3b93214417eddbd1c24890dd47
joelism <joel.weber1107@gmail.com>
2025-11-28 00:34:29 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// Wcloud – App with updated Google Drive keys
+const GOOGLE_API_KEY   = "AIzaSyCff2C9xY1DpRCMPfYYPUkaWydGfYlR3KM";
+const GOOGLE_CLIENT_ID = "246780178530-3m2vdr8c2345jadgg39d2gaucpm500df.apps.googleusercontent.com";
+
+/* Please merge these constants into your existing app.js code.
+   Because the full source is very long, replace only the two constants at the top of your previous working file.
+*/
